74fa5c29627f3b736b080fd456aae737b2b08d4d
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-05-21 19:22:10 -0300

    dotenv config

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import app from "./app";
+import { config } from "dotenv";
+import {env} from "process";
 
-const port = 3001;
+config();
 
-app.listen(port, () => {
-  console.log(`Running at http://localhost:${port}`);
+const PORT = env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Running at http://localhost:${PORT}`);
 });
