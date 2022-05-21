b4bed34c8c98ea80390f158d7f1a290ddf33e232
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-05-21 19:12:36 -0300

    message alert update

--- src/middlewares/verifyId.middleware.js
+++ src/middlewares/verifyId.middleware.js
@@ -5,7 +5,7 @@ export const verifyIdPaletaMiddleware = (req, res, next) => {
   const selectedPaleta = paletas.find((paleta) => paleta.id === id);
 
   if (!selectedPaleta) {
-    return res.status(404).send("A paleta não foi encontrada!");
+    return res.status(404).send({message:"A paleta não foi encontrada!"});
   }
 
   next();
