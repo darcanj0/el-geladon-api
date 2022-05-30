42bcf846039ba07a46ae572ac2070c01ef770183
Daniel Arcanjo <daniel.thomas.aarcanjo@gmail.com>
2022-05-30 11:36:07 -0300

    feat: connection string change MongoAtlas

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const { connect } = mongoose;
 
 export const connectToDatabase = () => {
-  connect("mongodb://localhost:27017/paletas-db", {
+  connect("mongodb+srv://root:admin@api-elgeladon.c8e0u.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -12,3 +12,6 @@ export const connectToDatabase = () => {
       console.log(`Error when connecting with MongoDB, Error: ${error}`)
     );
 };
+
+//local connection string
+//mongodb://localhost:27017/paletas-db
